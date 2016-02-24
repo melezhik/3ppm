aba63684bf7eb74d077603ca20360ca52916b93a
3party tests user <3ppm@ip-172-31-30-88.us-west-2.compute.internal>
2016-02-24 20:58:16 +0000

    update reporter

--- run-all.pl
+++ run-all.pl
@@ -1,14 +1,19 @@
-BEGIN { system("sparrow index update 2>/dev/null") };
+BEGIN { system("sparrow index update 2>/dev/null") unless $ENV{skip_test}};
 
 
 use Net::EmptyPort qw(empty_port);
+use Date::Format;
+use File::stat;
 
 next unless /\S+/;
 next if /^\s*#/;
 
+
 my ($p, $m ) = split;
 
 $tests{$p} = $m;
+
+next if $ENV{skip_test};
 
 print "running $p for $m ... \n";
 
@@ -25,17 +30,43 @@ END {
 
     open SUMMARY, ">", "/usr/share/3ppm/index.html" or die $!;
 
-    print SUMMARY "<table>\n";
 
-    print SUMMARY "<th> name </th> <th> module </th> status <th> link </th>\n";
+print SUMMARY <<HERE;
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+HERE
+
+    print SUMMARY "<table class='table table-stripped'>\n";
+
+    print SUMMARY "
+	<th> test suite </th> 
+	<th> module </th>
+	<th> status </th> 
+	<th> report </th>\n";
 
     for my $p (keys %tests){
-        open STATUS, "/usr/share/3ppm/$p.txt" or die $!;
-        my $st = <STATUS>;
-        close STATUS;
+        open my $status_fh, "/usr/share/3ppm/$p.status" or die $!;
+        my $st = <$status_fh>;
+        close $status_fh;
+	my @stat = stat("/usr/share/3ppm/$p.status");
+	my $check_date = time2str("%Y/%m/%d %T\n", $stat[0][9]);
         my $status =  ( $st == 0 ) ? 'OK' : 'FAIL';
+	my $st_class = ( $st == 0 ) ? 'success' : 'danger';
         print SUMMARY
-            "<tr> <td> $p </td> <td> $tests{$p} </td> <td> $status </td> <td> <a href='$p.txt'>report</a> </td> </tr> \n";
+            "<tr> 
+		<td> $p </td> 
+		<td> $tests{$p} </td> 
+		<td class='$st_class'> $status </td>
+		<td> <a href='$p.txt'>$check_date</a> </td> 
+	    </tr> \n";
     }
 
     print SUMMARY "</table>\n";
