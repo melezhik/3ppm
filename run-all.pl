9c2c869e9f6849b329f715fe2dd5769f8c9c1a0a
Alexey Melezhik <melezhik@gmail.com>
2016-02-25 14:46:32 +0300

    dealing with layout

--- run-all.pl
+++ run-all.pl
@@ -64,33 +64,36 @@ END {
 
 print SUMMARY <<HERE;
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-<head><title>cpanparty.org - Third Party Tests for CPAN modules</title></head>
+<head>
+    <title>cpanparty.org - Third Party Tests for CPAN modules</title>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
 
 HERE
 
-    print SUMMARY '
+    print SUMMARY<<HERE;
+
+
     <div class="container">
 	    <div class="panel panel-default">
             <div class="panel-heading">CPAN Party Test Reports </div>
             <div class="panel-body">
 	            <table class="table table-stripped">
-    	';
+HERE
 
-    print SUMMARY "
-	<th> check date </th> 
-	<th> test suite </th> 
-	<th> module </th>
-	<th> status </th> 
-	<th> report </th>\n";
+    print SUMMARY<<HERE;
+	    <th> check date </th> 
+    	<th> test suite </th> 
+	    <th> module </th>
+    	<th> status </th> 
+	    <th> report </th>
+    	<th> summary </th> 
+HERE
 
     for my $t (@tests){
 
@@ -106,15 +109,24 @@ HERE
         my $status =  ( $st == 0 ) ? 
 		'<span class="label label-success">Passed</span>' : 
 		'<span class="label label-danger">Failed</span>';
-	my $st_class = ( $st == 0 ) ? 'success' : 'danger';
-        print SUMMARY
-            "<tr> 
-		<td><small>$check_date</small></td> 
-		<td> $p </td> 
-		<td><strong> $m </strong> </td> 
-		<td> $status </td>
-		<td> <a href='$p.txt'>link</a> </td> 
-	    </tr> \n";
+   	    my $st_class = ( $st == 0 ) ? 'success' : 'danger';
+
+        open my $report_fh, "/usr/share/cpanparty/$p.txt" or die $!;
+        my $report_summary = join "<br>", (<$report_fh>)[-2];
+        close $report_fh;
+
+        print SUMMARY<<HERE;
+            <tr> 
+		        <td><small>$check_date</small></td> 
+		        <td> $p </td> 
+		        <td><strong> $m </strong> </td> 
+		        <td> $status </td>
+		        <td><a href="$p.txt" target="_blank">show</a></td> 
+                <td class="breadcrumb"><small>$report_summary</small><td>
+	         </tr>
+
+HERE
+
     }
 
     print SUMMARY "</table></div></div></div>\n";
